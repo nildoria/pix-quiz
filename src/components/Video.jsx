37e5a54d6111e206e89968aaddc844a43591b7a4
Sabbir Ahmed <sabbir.ahmed87@gmail.com>
2023-09-19 03:26:51 +0600

    Update

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,7 @@ function Video({ title, id, noq }) {
   return (
     <div className={classes.video}>
       <img
-        src={`http://img.youtube.com/vi/${id}/maxresdefault.jpg`}
+        src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`}
         alt={title}
       />
       <p>{title}</p>
